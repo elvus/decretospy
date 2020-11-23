f347164085a0663d2723d22e4bde8569e4420f6c
elvus <e.s95@hotmail.com>
2020-11-23 20:42:57 -0300

    Actualizaciones

--- decretosapp.py
+++ decretosapp.py
@@ -1,7 +1,10 @@
 from flask import Flask, Response, render_template
 from flask_cors import CORS
+from bson.json_util import dumps
+from requests.api import request
 
-from decretos import decretos, write_output
+from decretos import connection, write_output
+import urllib3
 
 app = Flask(__name__, static_url_path='')
 cors = CORS(app, resources={r"/api/*": {"origins":"*"}})
@@ -10,17 +13,25 @@ cors = CORS(app, resources={r"/api/*": {"origins":"*"}})
 def index():
     return render_template("index.html")
 
-@app.route("/api/v1/")
-
+@app.route("/api/all")
 def api_root():
-    response=""
-    try:
-        with open('decretos.json', 'r') as f:
-            response=f.read()
-    except IOError:
-        response=decretos()
-        write_output()
+    db=connection()
+    response=dumps(db.decretos.find())
     return Response(response=response, status=200, mimetype='application/json')
+
+@app.route("/api/<path:q>",methods=['GET', 'POST'])
+def query(q):
+    db=connection()
+    print(q)
+    response=dumps(db.decretos.find({
+        "$or":[
+                {'fecha': {'$regex': q}},
+                {'descripcion': {'$regex': str(urllib3.util.parse_url(q)), "$options" :'i'}}
+            ]
+        })
+    )
+    return Response(response=response, status=200, mimetype='application/json')
+
 
 if __name__=="__main__":
         app.run(debug=True)
